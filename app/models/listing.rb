e90f00983350a30136e0017c641a7096297a63ad
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-06 23:57:27 -0700

    adding figaro

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,10 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "missing.jpg" 
+	has_attached_file :image, 
+					  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
+					  :default_url => "missing.jpg",
+				      :storage => :dropbox,
+    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    	             # :dropbox_options => {...}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	#validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
 	#validates_attachment_content_type :image, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
