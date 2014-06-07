a5cc0cb114c2bb2aeb4805a8993b713112a4f3d1
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-07 00:38:23 -0700

    finish dropbox setup with if statement

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,20 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, 
-					  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
-					  :default_url => "missing.jpg",
-				      :storage => :dropbox,
-    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-    	             # :dropbox_options => {...}
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	#validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
-	#validates_attachment_content_type :image, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
-	#do_not_validate_attachment_file_type :image
+	if rails.env.development?
+		has_attached_file :image, 
+						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
+						  :default_url => "missing.jpg",
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	else
+		has_attached_file :image, 
+						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
+						  :default_url => "missing.jpg",
+					      :storage => :dropbox,
+	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	    				  :path => ":style/:id_:filename"
+	    	             # :dropbox_options => {...}
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+		#validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
+		#validates_attachment_content_type :image, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
+		#do_not_validate_attachment_file_type :image
+	end
 end
