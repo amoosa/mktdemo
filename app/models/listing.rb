c3199ec3c5b5b9dbd3e543696816cc17256f53f4
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-07 01:08:56 -0700

    reload

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,5 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, 
-						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
-						  :default_url => "missing.jpg"
-		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	else
+
 		has_attached_file :image, 
 						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
 						  :default_url => "missing.jpg",
@@ -16,5 +11,5 @@ class Listing < ActiveRecord::Base
 		#validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
 		#validates_attachment_content_type :image, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
 		#do_not_validate_attachment_file_type :image
-	end
+	#end
 end
