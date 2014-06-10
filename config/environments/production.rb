0bc20a2fa2857599b0b65bd6fad8329c4c7a61e0
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-09 21:40:43 -0700

    installed and configured devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Mktdemo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+# required for devise install.remember to change url to actual domain
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
 end
