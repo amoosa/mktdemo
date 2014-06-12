726838842d8622ca4bd1de726f6e7eaf5c1469f5
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-12 15:33:25 -0700

    linking orders to users and listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610062339) do
+ActiveRecord::Schema.define(version: 20140612185134) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 20140610062339) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "userid"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "listing_id"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
   end
 
   create_table "users", force: true do |t|
