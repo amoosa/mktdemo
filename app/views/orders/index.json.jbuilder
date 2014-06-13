5e22e4d2645f453b8f3db9419900fbcc241c5064
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-12 19:54:32 -0700

    dropdowns for sales and purch links

--- app/views/orders/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@orders) do |order|
-  json.extract! order, :id, :address, :city, :state, :zip
-  json.url order_url(order, format: :json)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Mktdemo::Application.routes.draw do
   
   devise_for :users
   resources :listings do
-    resources :orders
+    resources :orders, only: [:new, :create]
   end
 
-  get "pages/help"
-  get "pages/test"
   get "pages/about"
   get "pages/contact"
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
