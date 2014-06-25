5713c9e5541d4d383dd0e5e7388d7df629da44ca
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-06-25 12:30:40 -0700

    colorbox testing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,19 +2,19 @@ class Listing < ActiveRecord::Base
 
 	if Rails.env.development?
 		has_attached_file :image, 
-						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
-						  :default_url => "missing.jpg"
+						  :styles => { :medium => "200x>", :thumb => "100x100>" },
+						 						  :default_url => "missing.jpg"
 		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 		has_attached_file :image2, 
-						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
+						  :styles => { :medium => "200x>", :thumb => "100x100>" },
 						  :default_url => "missing.jpg"
 		validates_attachment_content_type :image2, :content_type => /\Aimage\/.*\Z/
 		has_attached_file :image3, 
-						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
+						  :styles => { :medium => "200x>", :thumb => "100x100>" },
 						  :default_url => "missing.jpg"
 		validates_attachment_content_type :image3, :content_type => /\Aimage\/.*\Z/
 		has_attached_file :image4, 
-						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
+						  :styles => { :medium => "200x>", :thumb => "100x100>" },
 						  :default_url => "missing.jpg"
 		validates_attachment_content_type :image4, :content_type => /\Aimage\/.*\Z/
 	else
@@ -28,8 +28,8 @@ class Listing < ActiveRecord::Base
 		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 		has_attached_file :image2, 
 						  :styles => { :medium => "200x>", :thumb => "100x100>" }, 
-						  :default_url => "missing.jpg",
-					      :storage => :dropbox,
+						  :default_url => "missing.jpg",					     
+						  :storage => :dropbox,
 	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	    				  :path => ":style/:id_:filename"
 	    	             # :dropbox_options => {...}
