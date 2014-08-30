56b5fd24764b03b89ff66a513201c1a12715d0ac
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-08-30 16:30:16 -0700

    navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,13 @@
 //= require_tree .
 
 $('.best_in_place').best_in_place()
+
+$(document).ready(function() {   
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+        });
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -65,7 +65,7 @@ class Listing < ActiveRecord::Base
           listing_hash = {:name => row['Product title'], 
           	              :description => row['Description'],
           				  :sku => row['Product_id'],
-  						  :price => row['Price'], :category => row['Category'.titleize], :inventory => row['Quantity in stock'],
+  						  :price => row['Price'], :category => row['Category'.downcase.titleize], :inventory => row['Quantity in stock'],
   						  :image => URI.parse(row['Image']),
   						  :user_id => user_id}.tap do |list_hash|
 						    list_hash[:image2] = URI.parse(row['Image2']) if row['Image2'] 
