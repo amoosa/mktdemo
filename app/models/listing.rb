9912fe1567000a737a63e176bdc219c1cea9cc29
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-09-03 09:16:47 -0700

    css

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,8 @@ class Listing < ActiveRecord::Base
 
 	if Rails.env.development?
 		has_attached_file :image, 
-						  :styles => { :medium => "200x200", :thumb => "100x100>" },
-						 						  :default_url => ""
+						  :styles => { :medium => "200x200", :thumb => "100x100" },
+						  :default_url => ""
 		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 		has_attached_file :image2, 
 						  :styles => { :medium => "200x200", :thumb => "100x100>" },
