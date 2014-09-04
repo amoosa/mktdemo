35398d3970adb79a3cd91d293899af14b89d73c4
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-09-04 11:26:45 -0700

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Mktdemo::Application.configure do
 # required for devise install.remember to change url to actual domain
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
