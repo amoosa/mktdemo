3779aae05704e7fc37b50a6233878412c8e5205e
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-09-04 11:56:07 -0700

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,15 +81,4 @@ Mktdemo::Application.configure do
 # required for devise install.remember to change url to actual domain
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  #:domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
-
 end
