32ecb228ad913d463c5675ffe3e5d1f216644a3d
awayo <basurillas.varias@gmail.com>
2014-09-24 14:10:12 +0200

    Change content type to filename validator

--- app/models/user_listing.rb
+++ app/models/user_listing.rb
@@ -1,7 +1,7 @@
 class UserListing < ActiveRecord::Base
   belongs_to :user
   has_attached_file :file
-  validates_attachment :file, content_type: { content_type: 'text/plain' }
+  validates_attachment_file_name :file, :matches => [/csv\Z/]
 
 
   def started!
