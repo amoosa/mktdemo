444a4f3d6b940afa264b04402372319a43f34617
Ash Moosa <ashfaaqmoosa@gmail.com>
2014-12-10 16:48:46 -0800

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Mktdemo::Application.configure do
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_protocol => :https,
+    :s3_permissions => :private,
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
