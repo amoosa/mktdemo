95ee7a8c3a84b0675c1dd001ee94e8a913b05e1a
Ash Moosa <ashfaaqmoosa@gmail.com>
2015-03-06 22:31:29 -0800

    serch

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -100,8 +100,12 @@ class ListingsController < ApplicationController
 
   def search
     if params[:search].present?
-      @listings = Listing.search(params[:search], where: { inventory: {gt: 0}, updated_at: {gte: (Date.current - 30.day)},
-                                           or: [ [{inventory: {gt: 0}}, user_id: {not: 24}] ] })
+
+      @listings = Listing.search(params[:search], where: { inventory: {gt: 0},
+                                           or: [updated_at: {gte: (Date.current - 30.day)}, user_id: {not: 24}] })
+
+     # @listings = Listing.search(params[:search], where: { inventory: {gt: 0}, updated_at: {gte: (Date.current - 30.day)},
+      #                                     or: [ [{inventory: {gt: 0}}, user_id: {not: 24}] ] })
      else
       @listings = Listing.order("created_at DESC")
     end
