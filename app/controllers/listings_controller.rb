7a16040771342bd61138b981e915d0b4713cab52
Ash Moosa <ashfaaqmoosa@gmail.com>
2015-03-06 21:32:10 -0800

    search

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -100,8 +100,7 @@ class ListingsController < ApplicationController
 
   def search
     if params[:search].present?
-      #@listings = Listing.search(params[:search], where: { inventory: {gt: 0} })
-      @listings = Listing.search(params[:search])
+      @listings = Listing.listable.search(params[:search], where: { inventory: {gt: 0} })
      else
       @listings = Listing.order("created_at DESC")
     end
